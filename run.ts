7030336241f1c8f1218838b707329dbe4bbf2f40
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-10-16 22:29:27 +0900

    List all eras and blocks

--- run.ts
+++ run.ts
@@ -7,10 +7,40 @@ import "./interfaces/augment-types";
 
 import type { u32 } from "@polkadot/types/primitive";
 
-const firstEraBlockNumber = 499296;
+const MILLISECS_PER_BLOCK = 12000;
+const MINUTES = 60000 / MILLISECS_PER_BLOCK;
+const HOURS = MINUTES * 60;
+const DAYS = HOURS * 24; // 7200 blocks
+
+const firstForceEraBlockNumber = 499296;
+const secondEraBlockNumber = 504001;
 
 const main = async () => {
-  const api = await ApiPromise.create({
+  const api = await getApi();
+  const currentBlock = await api.rpc.chain.getBlock();
+  const currentBlockNumber = currentBlock.block.header.number
+    .unwrap()
+    .toNumber();
+
+  const eraBlockArray = [
+    {
+      era: await checkAndGetEraByBlockNumber(api, firstForceEraBlockNumber),
+      block: firstForceEraBlockNumber,
+    },
+  ];
+
+  for (let i = secondEraBlockNumber; i <= currentBlockNumber; i += DAYS) {
+    eraBlockArray.push({
+      era: await checkAndGetEraByBlockNumber(api, i),
+      block: i,
+    });
+  }
+
+  console.log(eraBlockArray);
+};
+
+const getApi = () => {
+  return ApiPromise.create({
     provider: new WsProvider("wss://shiden.api.onfinality.io/public-ws"),
     types: {
       ...Object.values(definitions).reduce(
@@ -23,8 +53,13 @@ const main = async () => {
       BlockV0: "u8", // FIXME: not correct, but we don't use this, just for supressing warning
     },
   });
+};
 
-  const blockHash = await api.rpc.chain.getBlockHash(firstEraBlockNumber);
+const checkAndGetEraByBlockNumber = async (
+  api: ApiPromise,
+  blockNumber: number
+) => {
+  const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
   const atApi = await api.at(blockHash);
   const eventRecords = await atApi.query.system.events();
 
@@ -46,12 +81,7 @@ const main = async () => {
     throw new Error(`invalid eras len: ${eras}`);
   }
 
-  const era = eras[0];
-
-  console.log(era);
-
-  // const currentBlock = await api.rpc.chain.getBlock();
-  // const currentBlockNumber = currentBlock.block.header.number.unwrap().toNumber()
+  return eras[0];
 };
 
 main().catch(console.error).finally(process.exit);
