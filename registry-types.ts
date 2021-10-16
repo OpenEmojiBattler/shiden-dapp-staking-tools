859a51ad6453c79c1aee7aff349df0785cca2e62
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-10-16 14:15:11 +0900

    Get shiden types

---
+++ registry-types.ts
@@ -0,0 +1,28 @@
+import type { RegistryTypes } from "@polkadot/types/types";
+
+// https://github.com/polkadot-js/apps/blob/d31088d35b9b3642d11f95d8b8b76b92c552cd7f/packages/apps-config/src/api/spec/shiden.ts Apache-2.0
+
+export const registryTypes: RegistryTypes = {
+  Keys: "AccountId",
+  Address: "MultiAddress",
+  LookupSource: "MultiAddress",
+  AmountOf: "Amount",
+  Amount: "i128",
+  SmartContract: {
+    _enum: {
+      Evm: "H160",
+      Wasm: "AccountId",
+    },
+  },
+  EraStakingPoints: {
+    total: "Balance",
+    stakers: "BTreeMap<AccountId, Balance>",
+    formerStakedEra: "EraIndex",
+    claimedRewards: "Balance",
+  },
+  EraRewardAndStake: {
+    rewards: "Balance",
+    staked: "Balance",
+  },
+  EraIndex: "u32",
+};
