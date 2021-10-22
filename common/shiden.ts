cd35e5d787bd09c8cc51c8ad5465f2caf924428b
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-10-22 22:21:52 +0900

    Update first block

--- common/shiden.ts
+++ common/shiden.ts
@@ -1,3 +1,3 @@
 export const firstForceEraBlockNumber = 499296;
 export const secondEraBlockNumber = 504001;
-export const claimEnabledBlockNumber = 547184; // https://shiden.subscan.io/extrinsic/547184-2
+export const claimEnabledBlockNumber = 548869; // first claim https://shiden.subscan.io/extrinsic/548869-3
