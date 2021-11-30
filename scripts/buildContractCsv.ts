fc6bbf4ab3d3ea36abdd6201d7b3d314227073f2
tash-2s <81064017+tash-2s@users.noreply.github.com>
2021-11-30 11:23:34 +0900

    Specify era range as same as buildBonus

--- scripts/buildContractCsv.ts
+++ scripts/buildContractCsv.ts
@@ -10,17 +10,29 @@ import {
   balanceToSdnNumber,
   formatSDN,
   getContractAddressArg,
+  getEraArg,
 } from "../common/utils";
 
 const main = () => {
-  const contractAddress = getContractAddressArg(process.argv[2]);
+  const contract = getContractAddressArg(process.argv[2]);
 
-  const records = readEraRecordAndContractEraRecordFiles(contractAddress);
+  const startEra = getEraArg(process.argv[3]);
+  const endEra = getEraArg(process.argv[4]);
+  if (startEra >= endEra) {
+    throw new Error("invalid era args");
+  }
+
+  const targetRecords: EraRecordAndContractEraRecord[] = [];
+  for (const record of readEraRecordAndContractEraRecordFiles(contract)) {
+    if (startEra <= record.era && record.era <= endEra) {
+      targetRecords.push(record);
+    }
+  }
 
   mkdirSync("./dist", { recursive: true });
 
-  buildStakers(contractAddress, records);
-  buildDeveloper(contractAddress, records);
+  buildStakers(contract, targetRecords);
+  buildDeveloper(contract, targetRecords);
 };
 
 const buildStakers = (
